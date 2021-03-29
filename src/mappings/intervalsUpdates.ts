a26afb2a77161b0f3eb411e270232e0e69d8ad9f
jmakoff <juramak98@gmail.com>
2021-03-29 12:15:06 +0300

    fixed intervals

--- src/mappings/intervalsUpdates.ts
+++ src/mappings/intervalsUpdates.ts
@@ -1,14 +1,15 @@
 
 import {
   Pair1minData,
-  Pair3minData,
   Pair5minData,
+  Pair15minData,
   Pair30minData,
   Pair1hData,
-  Pair2hData,
-  Pair4hData,
+  Pair3hData,
   Pair6hData,
-  Pair1dData
+  Pair12hData,
+  Pair1dData,
+  Pair3dData
 } from './../types/schema'
 import { Entity, BigInt, BigDecimal, ethereum } from '@graphprotocol/graph-ts'
 import { Pair, Bundle, Token, UniswapFactory, UniswapDayData, PairDayData, TokenDayData } from '../types/schema'
@@ -16,14 +17,15 @@ import { ONE_BI, ZERO_BD, ZERO_BI, FACTORY_ADDRESS } from './helpers'
 
 export function updateIntervalsData (event: ethereum.Event): void{
   update1minData(event)
-  update3minData(event)
   update5minData(event)
+  update15minData(event)
   update30minData(event)
   update1hData(event)
-  update2hData(event)
-  update4hData(event)
+  update3hData(event)
   update6hData(event)
+  update12hData(event)
   update1dData(event)
+  update3dData(event)
 }
 
 
@@ -68,8 +70,8 @@ function update1minData (event: ethereum.Event): void {
     pairIntervalData.save()
 }
 
-function update3minData (event: ethereum.Event): void {
-    let intervalInSec = 180 //value of period in sec
+function update5minData (event: ethereum.Event): void {
+    let intervalInSec = 300 //value of period in sec
 
     let timestamp = event.block.timestamp.toI32()
     let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
@@ -80,9 +82,9 @@ function update3minData (event: ethereum.Event): void {
       .concat(BigInt.fromI32(intervalIndex).toString())
     let pair = Pair.load(event.address.toHexString())
 
-    let pairIntervalData = Pair3minData.load(intervalEntityID)
+    let pairIntervalData = Pair5minData.load(intervalEntityID)
     if (pairIntervalData === null) {
-      pairIntervalData = new Pair3minData (intervalEntityID)
+      pairIntervalData = new Pair5minData (intervalEntityID)
 
 
       pairIntervalData.startTimestamp = intervalStartUnix
@@ -109,8 +111,9 @@ function update3minData (event: ethereum.Event): void {
     pairIntervalData.save()
 }
 
-function update5minData (event: ethereum.Event): void {
-    let intervalInSec = 300 //value of period in sec
+
+function update15minData (event: ethereum.Event): void {
+    let intervalInSec = 900 //value of period in sec
 
     let timestamp = event.block.timestamp.toI32()
     let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
@@ -121,9 +124,9 @@ function update5minData (event: ethereum.Event): void {
       .concat(BigInt.fromI32(intervalIndex).toString())
     let pair = Pair.load(event.address.toHexString())
 
-    let pairIntervalData = Pair5minData.load(intervalEntityID)
+    let pairIntervalData = Pair15minData.load(intervalEntityID)
     if (pairIntervalData === null) {
-      pairIntervalData = new Pair5minData (intervalEntityID)
+      pairIntervalData = new Pair15minData (intervalEntityID)
 
 
       pairIntervalData.startTimestamp = intervalStartUnix
@@ -232,8 +235,8 @@ function update1hData (event: ethereum.Event): void {
     pairIntervalData.save()
 }
 
-function update2hData (event: ethereum.Event): void {
-    let intervalInSec = 7200 //value of period in sec
+function update3hData (event: ethereum.Event): void {
+    let intervalInSec = 10800 //value of period in sec
 
     let timestamp = event.block.timestamp.toI32()
     let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
@@ -244,50 +247,9 @@ function update2hData (event: ethereum.Event): void {
       .concat(BigInt.fromI32(intervalIndex).toString())
     let pair = Pair.load(event.address.toHexString())
 
-    let pairIntervalData = Pair2hData.load(intervalEntityID)
+    let pairIntervalData = Pair3hData.load(intervalEntityID)
     if (pairIntervalData === null) {
-      pairIntervalData = new Pair2hData (intervalEntityID)
-
-
-      pairIntervalData.startTimestamp = intervalStartUnix
-      pairIntervalData.maxTimestamp = timestamp
-      pairIntervalData.pair = pair.id
-      pairIntervalData.openToken1Price = pair.token0Price
-      pairIntervalData.openToken0Price = pair.token1Price
-      pairIntervalData.closeToken1Price = pair.token1Price
-      pairIntervalData.closeToken0Price = pair.token0Price
-      pairIntervalData.highToken1Price = pair.token1Price
-      pairIntervalData.highToken0Price = pair.token0Price
-      pairIntervalData.lowToken1Price = pair.token1Price
-      pairIntervalData.lowToken0Price = pair.token0Price
-    }
-    if (pairIntervalData.maxTimestamp < timestamp){
-      pairIntervalData.maxTimestamp = timestamp
-      pairIntervalData.closeToken0Price = pair.token0Price
-      pairIntervalData.closeToken1Price = pair.token1Price
-    }
-    pairIntervalData.highToken0Price = pairIntervalData.highToken0Price > pair.token0Price ?  pairIntervalData.highToken0Price : pair.token0Price
-    pairIntervalData.highToken1Price = pairIntervalData.highToken1Price > pair.token1Price ?  pairIntervalData.highToken1Price : pair.token1Price
-    pairIntervalData.lowToken0Price = pairIntervalData.lowToken0Price < pair.token0Price ?  pairIntervalData.lowToken0Price : pair.token0Price
-    pairIntervalData.lowToken1Price = pairIntervalData.lowToken1Price < pair.token1Price ?  pairIntervalData.lowToken1Price : pair.token1Price
-    pairIntervalData.save()
-}
-
-function update4hData (event: ethereum.Event): void {
-    let intervalInSec = 14400 //value of period in sec
-
-    let timestamp = event.block.timestamp.toI32()
-    let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
-    let intervalStartUnix = intervalIndex * intervalInSec // want the rounded effect
-    let intervalEntityID = event.address
-      .toHexString()
-      .concat('-')
-      .concat(BigInt.fromI32(intervalIndex).toString())
-    let pair = Pair.load(event.address.toHexString())
-
-    let pairIntervalData = Pair4hData.load(intervalEntityID)
-    if (pairIntervalData === null) {
-      pairIntervalData = new Pair4hData (intervalEntityID)
+      pairIntervalData = new Pair3hData (intervalEntityID)
 
 
       pairIntervalData.startTimestamp = intervalStartUnix
@@ -355,6 +317,47 @@ function update6hData (event: ethereum.Event): void {
     pairIntervalData.save()
 }
 
+function update12hData (event: ethereum.Event): void {
+    let intervalInSec = 43200 //value of period in sec
+
+    let timestamp = event.block.timestamp.toI32()
+    let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
+    let intervalStartUnix = intervalIndex * intervalInSec // want the rounded effect
+    let intervalEntityID = event.address
+      .toHexString()
+      .concat('-')
+      .concat(BigInt.fromI32(intervalIndex).toString())
+    let pair = Pair.load(event.address.toHexString())
+
+    let pairIntervalData = Pair12hData.load(intervalEntityID)
+    if (pairIntervalData === null) {
+      pairIntervalData = new Pair12hData (intervalEntityID)
+
+
+      pairIntervalData.startTimestamp = intervalStartUnix
+      pairIntervalData.maxTimestamp = timestamp
+      pairIntervalData.pair = pair.id
+      pairIntervalData.openToken1Price = pair.token0Price
+      pairIntervalData.openToken0Price = pair.token1Price
+      pairIntervalData.closeToken1Price = pair.token1Price
+      pairIntervalData.closeToken0Price = pair.token0Price
+      pairIntervalData.highToken1Price = pair.token1Price
+      pairIntervalData.highToken0Price = pair.token0Price
+      pairIntervalData.lowToken1Price = pair.token1Price
+      pairIntervalData.lowToken0Price = pair.token0Price
+    }
+    if (pairIntervalData.maxTimestamp < timestamp){
+      pairIntervalData.maxTimestamp = timestamp
+      pairIntervalData.closeToken0Price = pair.token0Price
+      pairIntervalData.closeToken1Price = pair.token1Price
+    }
+    pairIntervalData.highToken0Price = pairIntervalData.highToken0Price > pair.token0Price ?  pairIntervalData.highToken0Price : pair.token0Price
+    pairIntervalData.highToken1Price = pairIntervalData.highToken1Price > pair.token1Price ?  pairIntervalData.highToken1Price : pair.token1Price
+    pairIntervalData.lowToken0Price = pairIntervalData.lowToken0Price < pair.token0Price ?  pairIntervalData.lowToken0Price : pair.token0Price
+    pairIntervalData.lowToken1Price = pairIntervalData.lowToken1Price < pair.token1Price ?  pairIntervalData.lowToken1Price : pair.token1Price
+    pairIntervalData.save()
+}
+
 function update1dData (event: ethereum.Event): void {
     let intervalInSec = 86400 //value of period in sec
 
@@ -370,6 +373,48 @@ function update1dData (event: ethereum.Event): void {
     let pairIntervalData = Pair1dData.load(intervalEntityID)
     if (pairIntervalData === null) {
       pairIntervalData = new Pair1dData (intervalEntityID)
+
+
+      pairIntervalData.startTimestamp = intervalStartUnix
+      pairIntervalData.maxTimestamp = timestamp
+      pairIntervalData.pair = pair.id
+      pairIntervalData.openToken1Price = pair.token0Price
+      pairIntervalData.openToken0Price = pair.token1Price
+      pairIntervalData.closeToken1Price = pair.token1Price
+      pairIntervalData.closeToken0Price = pair.token0Price
+      pairIntervalData.highToken1Price = pair.token1Price
+      pairIntervalData.highToken0Price = pair.token0Price
+      pairIntervalData.lowToken1Price = pair.token1Price
+      pairIntervalData.lowToken0Price = pair.token0Price
+    }
+    if (pairIntervalData.maxTimestamp < timestamp){
+      pairIntervalData.maxTimestamp = timestamp
+      pairIntervalData.closeToken0Price = pair.token0Price
+      pairIntervalData.closeToken1Price = pair.token1Price
+    }
+    pairIntervalData.highToken0Price = pairIntervalData.highToken0Price > pair.token0Price ?  pairIntervalData.highToken0Price : pair.token0Price
+    pairIntervalData.highToken1Price = pairIntervalData.highToken1Price > pair.token1Price ?  pairIntervalData.highToken1Price : pair.token1Price
+    pairIntervalData.lowToken0Price = pairIntervalData.lowToken0Price < pair.token0Price ?  pairIntervalData.lowToken0Price : pair.token0Price
+    pairIntervalData.lowToken1Price = pairIntervalData.lowToken1Price < pair.token1Price ?  pairIntervalData.lowToken1Price : pair.token1Price
+    pairIntervalData.save()
+}
+
+
+function update3dData (event: ethereum.Event): void {
+    let intervalInSec = 259200 //value of period in sec
+
+    let timestamp = event.block.timestamp.toI32()
+    let intervalIndex = timestamp /  intervalInSec // get unique interval within unix history
+    let intervalStartUnix = intervalIndex * intervalInSec // want the rounded effect
+    let intervalEntityID = event.address
+      .toHexString()
+      .concat('-')
+      .concat(BigInt.fromI32(intervalIndex).toString())
+    let pair = Pair.load(event.address.toHexString())
+
+    let pairIntervalData = Pair3dData.load(intervalEntityID)
+    if (pairIntervalData === null) {
+      pairIntervalData = new Pair3dData (intervalEntityID)
 
 
       pairIntervalData.startTimestamp = intervalStartUnix
